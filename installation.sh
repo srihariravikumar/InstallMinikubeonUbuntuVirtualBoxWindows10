eee5b813313b8502c6e8a619f4b4eab15cc123d8
KARTHEEK <KARTHEEKVADDE@users.noreply.github.com>
2020-06-21 14:11:51 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -30,7 +30,10 @@ sudo apt-get install -y virtualbox virtualbox-ext-pack
  sudo apt-get install -y conntrack
 
  #e. finally we are done with the installation and now start the minikube
- sudo minikube start --vm-driver=none
+// sudo minikube start --vm-driver=none
+//Add user to Docker Group
+sudo usermod -aG docker $USER
+minikube start --vm-driver=docker //docker driver
 
  #f. start the dashboard
  sudo minikube dashboard
